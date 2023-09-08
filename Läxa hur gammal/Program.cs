01d3183e62c19894cb131835d9504259f84dfb43
7R1XY <theo.broden@gmail.com>
2023-09-08 14:21:22 +0200

    Add project files.

---
+++ Läxa hur gammal/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Läxa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vilket år är du född");
+            string årtal = Console.ReadLine();
+            int x = int.Parse(årtal);
+
+            int ålder = 2023 - x;
+            Console.WriteLine("Du är " + ålder + " år gammal");
+            Console.ReadKey();
+
+
+        }
+    }
+}
